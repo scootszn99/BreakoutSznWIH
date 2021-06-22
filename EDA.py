1ca56753dca1a3468c74d09e6bb916ad967cb957
Nayan <nayanptl99@gmail.com>
2021-06-22 17:23:32 -0500

    Initial commit

---
+++ EDA.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+import os
+
+# reading in data
+df = pd.read_csv("pick224data.csv")
+
+# sorting dataframe by player and season to get groupings
+df = df.sort_values(['UNIQUE ID', 'SEASON'], kind='mergesort')
+
+# creating a new variable for each row flagging if it's a breakout season
+df['Breakout Season'] = np.where(df['P1/e60'] >= df['P1/e60'].shift(1) + 0.5, True, False)
+df.loc[df['UNIQUE ID'] != df['UNIQUE ID'].shift(1), "Breakout Season"] = False
+df.to_csv('breakoutcalculator.csv', index=False)
