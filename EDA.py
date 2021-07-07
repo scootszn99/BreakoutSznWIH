a0213453e96d191bfc5f1d02f2e65cc83f242791
Nayan <nayanptl99@gmail.com>
2021-07-06 19:32:39 -0500

    calculated breakout seasons

--- EDA.py
+++ EDA.py
@@ -12,4 +12,13 @@ df = df.sort_values(['UNIQUE ID', 'SEASON'], kind='mergesort')
 # creating a new variable for each row flagging if it's a breakout season
 df['Breakout Season'] = np.where(df['P1/e60'] >= df['P1/e60'].shift(1) + 0.5, True, False)
 df.loc[df['UNIQUE ID'] != df['UNIQUE ID'].shift(1), "Breakout Season"] = False
-df.to_csv('breakoutcalculator.csv', index=False)
+
+df = df.reset_index(drop=True)
+indices = df[df['Breakout Season'] == True].index-1
+df = df.loc[indices]
+df = df[df['POS'] == "F"]
+df = df.drop_duplicates(subset=['UNIQUE ID'])
+
+df
+
+# df.to_csv('breakoutcalculator.csv', index=False)
